b5c9a604abae23da3e3db15ebd95445db7c8e708
Aaloklowanshi <lowanshiaalok@gmail.com>
2026-03-01 14:16:07 +0530

    spring template

---
+++ docman_backend/src/test/java/com/docman/docman/DocmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docman.docman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
